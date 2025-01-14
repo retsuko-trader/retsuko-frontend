9273d61bc88afb1d91c001780c515cc1df29a2fe
20chan <2@0chan.dev>
2025-01-15 00:10:53 +0900

    import tulind - tulip indicator

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['duckdb', 'duckdb-async'],
+  serverExternalPackages: ['duckdb', 'duckdb-async', 'tulind'],
   /* config options here */
 };
 
